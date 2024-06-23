a12a07de7dfef4a122a7e93bb38c6287bcbb724b
lukablaskovic <lukablaskovic2000@gmail.com>
2024-06-23 12:41:36 +0200

    early gameplay available

--- snake/GameState.hs
+++ snake/GameState.hs
@@ -1,8 +1,8 @@
 module GameState where
 
 import System.Random
-import Apple (newApple) -- Import only newApple from Apple
-import Config -- Import the Config module
+import Apple (newApple)
+import Config
 
 -- Define the initial state of the game
 data GameState = GameState
@@ -10,6 +10,7 @@ data GameState = GameState
   , direction :: (Float, Float) -- Current direction of the snake
   , apple :: (Float, Float)     -- Position of the apple
   , rng :: StdGen               -- Random number generator
+  , timer :: Float              -- Timer to track time for apple respawn
   } deriving Show
 
 -- Initial game state
@@ -19,18 +20,28 @@ initialState = GameState
   , direction = (cellSize, 0)
   , apple = fst $ newApple (mkStdGen 42)
   , rng = mkStdGen 42
+  , timer = 0
   }
 
 -- Move the snake in the current direction
-moveSnake :: GameState -> GameState
-moveSnake gameState
-  | checkCollision newHead = gameState -- If collision, do not update the snake
-  | otherwise = gameState { snake = newSnake }
+moveSnake :: GameState -> [(Float, Float)]
+moveSnake gameState = newHead : init (snake gameState)
   where
     (dx, dy) = direction gameState
     newHead = (\(x, y) -> (x + dx, y + dy)) (head $ snake gameState)
-    newSnake = newHead : init (snake gameState)
-    checkCollision (x, y) = x < -fromIntegral windowWidth / 2 + cellSize
-                         || x >= fromIntegral windowWidth / 2 - cellSize
-                         || y < -fromIntegral windowHeight / 2 + cellSize
-                         || y >= fromIntegral windowHeight / 2 - cellSize
+
+-- Check if the snake eats the apple
+snakeEatsApple :: GameState -> Bool
+snakeEatsApple gameState = head (snake gameState) == apple gameState
+
+-- Check for snake collision with walls or itself
+checkCollision :: (Float, Float) -> Bool
+checkCollision (x, y) = x < -fromIntegral windowWidth / 2 + cellSize
+                     || x >= fromIntegral windowWidth / 2 - cellSize
+                     || y < -fromIntegral windowHeight / 2 + cellSize
+                     || y >= fromIntegral windowHeight / 2 - cellSize
+
+-- Grow the snake when it eats an apple
+growSnake :: [(Float, Float)] -> [(Float, Float)]
+growSnake (x:xs) = x : x : xs
+growSnake [] = []
--- snake/main.hs
+++ snake/main.hs
@@ -2,7 +2,8 @@ module Main where
 
 import Graphics.Gloss
 import Graphics.Gloss.Interface.Pure.Game
-import GameState
+import GameState (GameState(..), initialState, checkCollision, growSnake, snakeEatsApple) -- Import specific functions
+import qualified GameState as GS -- Qualified import for GameState to use its moveSnake
 import Render
 import Input
 import Apple
@@ -19,4 +20,21 @@ main = do
 
 -- Update the game state
 update :: Float -> GameState -> GameState
-update _ gameState = moveSnake gameState
+update seconds gameState = handleAppleRespawn . handleSnakeMovement $ gameState { timer = timer gameState + seconds }
+
+-- Handle snake movement and collision
+handleSnakeMovement :: GameState -> GameState
+handleSnakeMovement gameState
+  | snakeEatsApple gameState = gameState { snake = growSnake (snake gameState), apple = fst $ newApple (rng gameState), rng = snd $ newApple (rng gameState), timer = 0 }
+  | checkCollision (head newSnake) = gameState
+  | otherwise = gameState { snake = newSnake }
+  where
+    newSnake = GS.moveSnake gameState
+
+-- Handle apple respawn based on timer
+handleAppleRespawn :: GameState -> GameState
+handleAppleRespawn gameState
+  | timer gameState >= 10 = gameState { apple = newApplePos, rng = newRng, timer = 0 }
+  | otherwise = gameState
+  where
+    (newApplePos, newRng) = newApple (rng gameState)
